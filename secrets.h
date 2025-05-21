84ae671597eec762584b244b52672fbbd93ef137
Md. Shaifulla Zibon <86222698+zibontaroka@users.noreply.github.com>
2025-05-21 17:42:10 +0600

    secrets.h

---
+++ secrets.h
@@ -0,0 +1,17 @@
+/* Fill in information from Blynk Device Info here */
+
+// Blynk Template ID: Unique identifier for your Blynk project, found in the Blynk Cloud dashboard.
+#define BLYNK_TEMPLATE_ID "TMfkKC10R_8"
+
+// Blynk Template Name: Descriptive name for your Blynk project, displayed in the Blynk app.
+#define BLYNK_TEMPLATE_NAME "IOT Health Assistant "
+
+// Blynk Authentication Token: Secret key provided by Blynk to authenticate your device. Replace "Secrate keyy" with your actual token from the Blynk app or dashboard.
+#define BLYNK_AUTH_TOKEN "Secrate keyy"
+
+
+// WiFi SSID: The name of your WiFi network. Replace "WiFi" with your actual WiFi network name.
+char ssid[] = "WiFi";
+
+// WiFi Password: The password for your WiFi network. Replace "Password" with your actual WiFi password.
+char pass[] = "Password";
